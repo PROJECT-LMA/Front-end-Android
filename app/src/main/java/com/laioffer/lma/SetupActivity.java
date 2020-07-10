e50e3d0b7abbcdb3e85bd713c8a6f6a2609bf02a
ULTRACOLA <davidzero1107@gmail.com>
2020-07-10 14:56:08 +0800

    Update SetupActivity.java

--- app/src/main/java/com/laioffer/lma/SetupActivity.java
+++ app/src/main/java/com/laioffer/lma/SetupActivity.java
@@ -2,5 +2,34 @@ package com.laioffer.lma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SetupActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SetUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner dropdown = findViewById(R.id.spinner1);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+    }
+
+    public void buttonOnClick(View view) {
+        Button button = (Button) view;
+        Toast toast = Toast.makeText(this, "Region selected.", Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
