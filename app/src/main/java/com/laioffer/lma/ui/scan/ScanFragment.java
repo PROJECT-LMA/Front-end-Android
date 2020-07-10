c31fa5ed5f60c9ccc9d74f7372777ad332217147
huangx11 <huangxingwei1995@gmail.com>
2020-07-09 21:14:25 -0700

    test1

--- app/src/main/java/com/laioffer/lma/ui/scan/ScanFragment.java
+++ app/src/main/java/com/laioffer/lma/ui/scan/ScanFragment.java
@@ -1,10 +1,12 @@
 package com.laioffer.lma.ui.scan;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +14,24 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.laioffer.lma.MainActivity;
 import com.laioffer.lma.R;
 
-public class ScanFragment extends Fragment {
+import org.w3c.dom.Text;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanFragment extends Fragment implements ZXingScannerView.ResultHandler {
     private ScanViewModel scanViewModel;
+
+    private ZXingScannerView mScannerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,12 +39,50 @@ public class ScanFragment extends Fragment {
                 ViewModelProviders.of(this).get(ScanViewModel.class);
         View root = inflater.inflate(R.layout.fragment_scan, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
+
+        final ZXingScannerView zxscan = root.findViewById(R.id.zxscan);
+        super.onCreate(savedInstanceState);
+
+
+        Dexter.withActivity(getActivity())
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        zxscan.setResultHandler(ScanFragment.this);
+                        zxscan.startCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(getActivity(), "You must accept to continue!", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                });
+
         scanViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+
         return root;
+    }
+
+    @Override
+    public void onDestroy() {
+        mScannerView.stopCamera();
+        super.onDestroy();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+
     }
 }
