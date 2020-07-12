0c0e88bcee7d2a890473011dc9e5e06482e45c83
Yangyizhou0302 <yangyizhou@gwu.edu>
2020-07-12 16:19:16 -0400

    -framework of login and sign up

--- app/src/main/java/com/laioffer/lma/network/Account.java
+++ app/src/main/java/com/laioffer/lma/network/Account.java
@@ -10,7 +10,6 @@ import java.net.URL;
 public class Account {
 
     public static class AccountResult {
-
         private final boolean status;
         private final String message;
 
@@ -26,6 +25,43 @@ public class Account {
         public String getMessage() {
             return message;
         }
+    }
+    //validate login account
+    public static AccountResult userLogin(String password, String email){
+        AccountResult result;
+        HttpURLConnection conn = null;
+
+        try{
+            URL url = new URL(HttpUtils.serverUrl + HttpUtils.login);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+            JSONObject object = new JSONObject();
+            object.put("email", email);
+            object.put("password", password);
+
+            HttpUtils.writeJsonObject(conn, object);
+            conn.connect();
+
+            JSONObject response = HttpUtils.readJsonObjectFromResponse(conn);
+            Log.d("net", response.toString());
+
+            if (response.getBoolean("isSuccess")) {
+                result = new AccountResult(true, "Successfully login");
+            }else{
+                result = new AccountResult(false, "login fail");
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+            result = new AccountResult(false, "network error");
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        return result;
     }
 
     public static AccountResult userRegister(String firstName, String lastName, String password,
--- app/src/main/java/com/laioffer/lma/ui/account/LoginFragment.java
+++ app/src/main/java/com/laioffer/lma/ui/account/LoginFragment.java
@@ -1,14 +1,25 @@
 package com.laioffer.lma.ui.account;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.laioffer.lma.MainActivity;
 import com.laioffer.lma.R;
+import com.laioffer.lma.network.Account;
+import com.laioffer.lma.utils.EditTextValidator;
 
 
 public class LoginFragment extends Fragment {
@@ -28,7 +39,91 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View inflatedView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        /* get all fields */
+        final Button loginBtn = inflatedView.findViewById(R.id.login);
+        final EditText email = inflatedView.findViewById(R.id.username);
+        final EditText password = inflatedView.findViewById(R.id.password);
+
+        // disable button until all fields are filled correctly
+        loginBtn.setClickable(false);
+
+        email.addTextChangedListener(new EditTextValidator(email) {
+            @Override
+            public void validate(final TextView textView, String text) {
+                if (text == null || text.length() == 0) {
+                    textView.setError("Please enter your email address");
+                    loginBtn.setClickable(false);
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
+                    textView.setError("Not a valid email address");
+                    loginBtn.setClickable(false);
+                } else if (text.trim().isEmpty()) {
+                    textView.setError("Not a valid email address");
+                    loginBtn.setClickable(false);
+                }
+            }
+        });
+
+        password.addTextChangedListener(new EditTextValidator(password) {
+            @Override
+            public void validate(TextView textView, String text) {
+                if (text == null || text.length() < 6) {
+                    textView.setError("Please must be at least 6 characters");
+                    loginBtn.setClickable(false);
+                } else {
+                    loginBtn.setClickable(true);
+                }
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (password.getText().toString().isEmpty() ||
+                        email.getText().toString().isEmpty()) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getContext(), "Please fill all fields", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    return;
+                }
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final Account.AccountResult result = Account.userLogin(
+                                password.getText().toString(),
+                                email.getText().toString());
+                        Activity activity = getActivity();
+
+                        activity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getContext(), result.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                        if (result.isStatus()) {
+                            activity.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ViewPager pager = (ViewPager)getActivity().findViewById(R.id.account_viewpager);
+                                    pager.setCurrentItem(0, true);
+                                }
+                            });
+                        }
+                    }
+                });
+                thread.start();
+                startActivity(new Intent(getActivity(), MainActivity.class));
+            }
+        });
+
+        return inflatedView;
     }
+
 }
