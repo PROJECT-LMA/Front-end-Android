89de3e13c8f4c63e6074ceb6077c5348894a55b1
Doris <hpan996@gmail.com>
2020-07-28 17:46:33 -0700

    loacation and frame setup, recycler view is next

--- app/src/main/java/com/laioffer/lma/ui/overview/OverviewFragment.java
+++ app/src/main/java/com/laioffer/lma/ui/overview/OverviewFragment.java
@@ -3,6 +3,7 @@ package com.laioffer.lma.ui.overview;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.laioffer.lma.R;
@@ -25,29 +28,43 @@ public class OverviewFragment extends Fragment {
 
     private SwipeRefreshLayout swipeRefreshLayout;
     //private TextView totalAvailable;
+    private TextView location_name;
     private TextView washer_count_text;
+    private TextView dryer_count_text;
     private TextView dryerAvailable;
     private int num = 0;
     private User user;
-
+    private RecyclerView washer_recyclerView;
+    private RecyclerView.Adapter washerAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         user = User.getInstance(getContext());
         View root = inflater.inflate(R.layout.fragment_overview, container, false);
+        washer_recyclerView = (RecyclerView) root.findViewById(R.id.washer_recycler_list);
         swipeRefreshLayout = (SwipeRefreshLayout) root.findViewById(R.id.swipe);
+        location_name = root.findViewById(R.id.location_bar);
         washer_count_text = root.findViewById(R.id.washer_count);
+        dryer_count_text = root.findViewById(R.id.dryer_count);
         //washerAvailable = root.findViewById(R.id.washer);
         //dryerAvailable = root.findViewById(R.id.dryer);
         swipeRefreshLayout = (SwipeRefreshLayout) root.findViewById(R.id.swipe);
+
+
+        //initialize Screen
+        location_name.setText("Laundry Room : " + user.getLocation().getName());
         loadMachines();
 
+
+        //swipe update
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
+                        location_name.setText("Laundry Room : " + user.getLocation().getName());
                         loadMachines();
                         swipeRefreshLayout.setRefreshing(false);
                     }
@@ -57,6 +74,8 @@ public class OverviewFragment extends Fragment {
 
         return root;
     }
+
+
 
     private void loadMachines() {
 
@@ -78,9 +97,11 @@ public class OverviewFragment extends Fragment {
                 threadHandler.post(new Runnable() {
                     @Override
                     public void run() {
+
                         Washers washers = getWashers(list);
                         Dryers dryers = getDryers(list);
-                        washer_count_text.setText(washers.availableCount + " of " + washers.totalCount + "available");
+                        washer_count_text.setText(washers.availableCount + " of " + washers.totalCount + " available");
+                        dryer_count_text.setText(dryers.availableCount + " of " + dryers.totalCount + " available");
                         list.clear();
                     }
                 });
@@ -93,11 +114,14 @@ public class OverviewFragment extends Fragment {
     private Washers getWashers(List<Machine> list){
         Washers washers = new Washers();
         for (Machine m : list){
-            if (m.getIsAvailable().equals("true") && m.getMachineType().equals("washer")){
-                washers.availableCount++;
-                washers.list.add(m);
+            if (m.getMachineType().equals("washer")){
+                if(m.getIsAvailable().equals("true")) {
+                    washers.availableCount++;
+                    washers.list.add(m);
+                }
+                washers.totalCount++;
             }
-            washers.totalCount++;
+            Log.d("machines", m.getMachineType());
         }
         return washers;
     }
@@ -105,22 +129,24 @@ public class OverviewFragment extends Fragment {
     private Dryers getDryers(List<Machine> list){
         Dryers dryers = new Dryers();
         for (Machine m : list){
-            if (m.getIsAvailable().equals("true") && m.getMachineType().equals("washer")){
-                dryers.availableCount++;
-                dryers.list.add(m);
+            if (m.getMachineType().equals("dryer")){
+                if(m.getIsAvailable().equals("true")) {
+                    dryers.availableCount++;
+                    dryers.list.add(m);
+                }
+                dryers.totalCount++;
             }
-            dryers.totalCount++;
         }
         return dryers;
     }
 
-    private static class Washers{
+    public class Washers{
         List<Machine> list = new ArrayList<>();
         int availableCount = 0;
         int totalCount = 0;
     }
 
-    private static class Dryers{
+    public class Dryers{
         List<Machine> list = new ArrayList<>();
         int availableCount = 0;
         int totalCount = 0;
